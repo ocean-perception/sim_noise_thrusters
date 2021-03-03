c2df7d99ac188938f281159ac2c2ada5061c397e
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-03-03 16:00:26 +0200

    Randomizing all thruster setpoints

--- src/sim_noise_thrusters/sim_noise_thrusters.py
+++ src/sim_noise_thrusters/sim_noise_thrusters.py
@@ -32,8 +32,9 @@ class ThrusterNoiseGenerator:
         randomness = [0, 0, 0]
 
         if diff_sec > 10*60: # frequncy of fault occurance every 10min
-            randomness[0] = - msg.setpoints[0]
-            randomness[1] = - msg.setpoints[1]
+            randomness[0] = round(random.uniform(-1, 1), 3)
+            randomness[1] = round(random.uniform(-1, 1), 3)
+            randmoness[2] = round(random.uniform(-1, 1), 3)
             
         new_msg.setpoints[0] = msg.setpoints[0] + randomness[0]
         new_msg.setpoints[1] = msg.setpoints[1] + randomness[1]

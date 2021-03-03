6ec1765c1ab517fa406f260530a74a3defa2d12e
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-03-03 18:50:30 +0200

    Setting thruster setpoint [1] to 0.

--- src/sim_noise_thrusters/sim_noise_thrusters.py
+++ src/sim_noise_thrusters/sim_noise_thrusters.py
@@ -32,10 +32,8 @@ class ThrusterNoiseGenerator:
         randomness = [0, 0, 0]
 
         if diff_sec > 10*60: # frequncy of fault occurance every 10min
-            randomness[0] = round(random.uniform(-1, 1), 3)
-            randomness[1] = round(random.uniform(-1, 1), 3)
-            randomness[2] = round(random.uniform(-1, 1), 3)
-            
+            randomness[1] = - msg.setpoints[1]
+                        
         new_msg.setpoints[0] = msg.setpoints[0] + randomness[0]
         new_msg.setpoints[1] = msg.setpoints[1] + randomness[1]
         new_msg.setpoints[2] = msg.setpoints[2] + randomness[2]
